4745a9709fafdf3d13563df5d695e1168e97bcc6
Pieter Verberck <pieter.verberck@inthepocket.mobi>
2024-03-26 13:48:53 +0100

    simple server

--- src/main.go
+++ src/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "log"
+import (
+	"errors"
+	"log"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, _ *http.Request) {
+	log.Println("received GET /hello")
+
+	w.Write([]byte("Hello world from server"))
+
+}
 
 func main() {
+
 	log.Println("Hello world")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", handlerFunc)
+
+	server := http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("Error running http server: %s\n", err)
+		}
+	}
+
 }
