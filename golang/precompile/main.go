422a12834d20b5620cdc88d8fd2450661322740c
Pieter Verberck <pieter.verberck@inthepocket.mobi>
2024-03-26 22:56:40 +0100

    optimize build

---
+++ golang/precompile/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	json.Marshal("")
+	fmt.Println("")
+	log.Println()
+	url.Parse("")
+	os.IsPathSeparator(100)
+	filepath.Clean("")
+	godotenv.Load()
+	http.CanonicalHeaderKey("")
+	errors.New("")
+	mongo.Connect(context.TODO(), options.Client().ApplyURI(""))
+}
